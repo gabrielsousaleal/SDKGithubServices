fef0d44e0d07c9dee57df6af6a7c517dd40f1822
Gabriel Sousa Leal <gabrielsousaleal@hotmail.com>
2021-04-12 10:04:27 -0300

    xcframework

---
+++ SDKGithubServices.xcframework/ios-arm64_x86_64-simulator/SDKGithubServices.framework/Headers/SDKGithubServices.h
@@ -0,0 +1,18 @@
+//
+//  SDKGithubServices.h
+//  SDKGithubServices
+//
+//  Created by Gabriel Sousa on 10/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKGithubServices.
+FOUNDATION_EXPORT double SDKGithubServicesVersionNumber;
+
+//! Project version string for SDKGithubServices.
+FOUNDATION_EXPORT const unsigned char SDKGithubServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKGithubServices/PublicHeader.h>
+
+
